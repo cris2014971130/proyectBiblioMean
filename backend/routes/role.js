a0c08659f42a8ba98e457bcd842ad8018a499596
“cris2014971130” <cris.2014971130@gmail.com>
2021-11-18 09:39:53 -0500

    Add Authorization and admin functions

--- backend/routes/role.js
+++ backend/routes/role.js
@@ -2,12 +2,6 @@ import express from "express";
 import role from "../controllers/role.js";
 const router = express.Router();
 
-// router.post("/registerRole", auth, admin, role.registerRole);
-// router.get("/listRole", auth, admin, role.listRole);
-// router.get("/findRole/:_id", auth, admin, role.findRole);
-// router.put("/updateRole", auth, admin, role.updateRole);
-// router.delete("/deleteRole/:_id", auth, admin, role.deleteRole);
-
 router.post("/registerRole",  role.registerRole);
 router.get("/listRole", role.listRole);
 router.get("/findRole/:_id",  role.findRole);
